fa484e6bbf6f63de0a1c45d6627a521152a09ca7
shubhamvsnv2 <72275611+shubhamvsnv2@users.noreply.github.com>
2020-10-03 00:38:28 +0530

    Update hello.c
    improving code with proper commenting !!

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
+// Simple C program to display "Hello World"
+
+// Header file for input output functions
 #include<studio.h>
+
+// main function - 
+// where the execution of program begins 
 void main()
 {
+// prints hello world  
 printf("hello world...");
 }
